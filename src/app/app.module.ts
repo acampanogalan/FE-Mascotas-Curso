c54deed75368c3f8d5673e4e002db5031dd8c7d5
Alejandro Campano <a.campano.galan@avanade.com>
2024-04-23 08:49:19 +0200

    Base proyect

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+//Components
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
+import { ListadoMascotasComponent } from './components/listado-mascotas/listado-mascotas.component';
+import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Angular Material
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AgregarEditarMascotaComponent,
+    ListadoMascotasComponent,
+    VerMascotaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
