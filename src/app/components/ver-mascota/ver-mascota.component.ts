d71b0868d831443ae5e6089490eef33cddf12fe8
acampanogalan <a.campano.galan@avanade.com>
2024-04-26 12:02:02 +0200

    ver mascotas done

--- src/app/components/ver-mascota/ver-mascota.component.ts
+++ src/app/components/ver-mascota/ver-mascota.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Mascota } from 'src/app/interfaces/mascota';
+import { MascotaService } from 'src/app/services/mascota.service';
 
 @Component({
   selector: 'app-ver-mascota',
@@ -6,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ver-mascota.component.css']
 })
 export class VerMascotaComponent implements OnInit {
+  id:number;
+  mascota!: Mascota;
+  loading: boolean = false;
+  // mascota$!: Observable<Mascota> ;
 
-  constructor() { }
+  constructor(private _mascotaService: MascotaService,
+    private aRoute: ActivatedRoute) {
+    this.id = parseInt(this.aRoute.snapshot.paramMap.get('id')!);
+  }
 
   ngOnInit(): void {
+    // this.mascota$ = this._mascotaService.getMascota(this.id);
+    this.obtenerMascota();
+  }
+
+  obtenerMascota() {
+    this.loading = true;
+    this._mascotaService.getMascota(this.id).subscribe({
+      next: (data) => {
+        this.mascota = data;
+        this.loading = false;
+      },
+      error: error => {
+        console.error(error);
+        alert('Ocurrio un error al obtener la mascota');
+      }
+    });
   }
 
 }
