5968a4aae659266e5d691e506fb7a42b380e36f3
acampanogalan <a.campano.galan@avanade.com>
2024-04-29 10:16:11 +0200

    put done

--- src/app/components/agregar-editar-mascota/agregar-editar-mascota.component.ts
+++ src/app/components/agregar-editar-mascota/agregar-editar-mascota.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Mascota } from '../../interfaces/mascota';
 import { MascotaService } from 'src/app/services/mascota.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-agregar-editar-mascota',
@@ -12,13 +12,15 @@ import { Router } from '@angular/router';
 })
 export class AgregarEditarMascotaComponent implements OnInit {
   loading: boolean = false;
-
-  form: FormGroup
+  id: number;
+  form: FormGroup;
+  operacion: string = 'Agregar';
 
   constructor(private fb: FormBuilder,
     private _mascotaService: MascotaService,
     private _snackBar: MatSnackBar,
-    private router: Router
+    private router: Router,
+    private aRoute: ActivatedRoute
   ) {
     this.form = this.fb.group({
       nombre: ['', Validators.required],
@@ -27,13 +29,37 @@ export class AgregarEditarMascotaComponent implements OnInit {
       color: ['', Validators.required],
       peso: ['', Validators.required]
     });
-
+    this.id = Number(this.aRoute.snapshot.paramMap.get('id'));
    }
 
   ngOnInit(): void {
+    if(this.id != 0){
+      this.operacion = 'Editar';
+      this.obtenerMascota(this.id);
+    }
   }
 
-  agregarMascota() {
+  obtenerMascota(id:number) {
+    this.loading = true;
+    this._mascotaService.getMascota(id).subscribe({
+      next: data => {
+        this.loading = false;
+        this.form.patchValue({
+          nombre: data.nombre,
+          edad: data.edad,
+          raza: data.raza,
+          color: data.color,
+          peso: data.peso
+        });
+      },
+      error: error => {
+        console.error(error);
+        alert('Ocurrio un error al obtener la mascota');
+      }
+    });
+  }
+
+  agregarEditarMascota() {
     // const nombre = this.form.get('nombre')?.value;
 
     const mascota: Mascota = {
@@ -44,9 +70,34 @@ export class AgregarEditarMascotaComponent implements OnInit {
       peso : this.form.value.peso
     }
 
+    if (this.id === 0) {
+      this.agregarMascota(mascota);
+    }
+    else {
+      mascota.id = this.id;
+      this.editarMascota(this.id, mascota);
+    }
+  }
+
+  editarMascota(id: number, mascota: Mascota) {
+    this.loading = true;
+    this._mascotaService.updateMascota(id, mascota).subscribe({
+      next: (data) => {
+        this.loading = false;
+        this.mensajeExito('editada');
+        this.router.navigate(['/listMascotas']);
+      },
+      error: error => {
+        console.error(error);
+        alert('Ocurrio un error al editar la mascota');
+      }
+    });
+  }
+
+  agregarMascota(mascota: Mascota){
     this._mascotaService.addNewMascota(mascota).subscribe({
       next: (data) => {
-        this.mensajeExito();
+        this.mensajeExito('registrada');
         this.router.navigate(['/listMascotas']);
       },
       error: error => {
@@ -56,8 +107,8 @@ export class AgregarEditarMascotaComponent implements OnInit {
     });
   }
 
-  mensajeExito() {
-    this._snackBar.open('La mascota fue registrada con exito', '', {
+  mensajeExito(text: string) {
+    this._snackBar.open(`La mascota fue ${text} con exito`, '', {
       duration: 3000,
       horizontalPosition: 'center',
       verticalPosition: 'top'
